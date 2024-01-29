224398e4d616d82a904b6e29017fd69e069275a7
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2024-01-29 12:48:28 +0530

    private key method

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 export const PrimaryGrey = "#828488";
-export const VAULT_ADDRESS = "0xC8f52445fbe5c341dc36E66153Ff1Ec528e6A7D4";
+export const VAULT_ADDRESS = "0x148F40E2462754CA7189c2eF33cFeD2916Ca1BC3";
--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -11,7 +11,7 @@ export async function createWallet(mnemonic) {
 		const wallet = ethers.Wallet.fromPhrase(mnemonic);
 
 		chrome.storage.local.set(
-			{ address: wallet.address, mnemonic },
+			{ address: wallet.address, mnemonic, privateKey: wallet.privateKey },
 			function () {
 				// Display the wallet address
 				console.log("Wallet Address:", wallet.address);
@@ -25,6 +25,15 @@ export async function getWallet() {
 		chrome.storage.local.get(["address"], (response) => {
 			if (!response.address) response.address = "";
 			res(response.address);
+		});
+	});
+}
+
+export async function getPrivateKey() {
+	return new Promise((res, rej) => {
+		chrome.storage.local.get(["privateKey"], (response) => {
+			if (!response.privateKey) response.privateKey = "";
+			res(response.privateKey);
 		});
 	});
 }
